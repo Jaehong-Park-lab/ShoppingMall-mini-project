30e359684a63a3750c1242deb53272cc1193da85
TheMoon <98293770+Jaehong-Park-lab@users.noreply.github.com>
2022-02-24 16:53:20 +0900

    Add files via upload

---
+++ Login/login.js
@@ -0,0 +1,31 @@
+// $(".email-signup").hide();
+// $("#signup-box-link").click(function(){
+//   $(".email-login").fadeOut(100);
+//   $(".email-signup").delay(100).fadeIn(100);
+//   $("#login-box-link").removeClass("active");
+//   $("#signup-box-link").addClass("active");
+// });
+// $("#login-box-link").click(function(){
+//   $(".email-login").delay(100).fadeIn(100);
+//   $(".email-signup").fadeOut(100);
+//   $("#login-box-link").addClass("active");
+//   $("#signup-box-link").removeClass("active");
+// });
+
+alert("0000");
+$(document).ready(function(){
+  alert("1111"); 
+//   $(".email-signup").hide();
+// $("#signup-box-link").click(function(){
+//   $(".email-login").fadeOut(100);
+//   $(".email-signup").delay(100).fadeIn(100);
+//   $("#login-box-link").removeClass("active");
+//   $("#signup-box-link").addClass("active");
+// });
+// $("#login-box-link").click(function(){
+//   $(".email-login").delay(100).fadeIn(100);
+//   $(".email-signup").fadeOut(100);
+//   $("#login-box-link").addClass("active");
+//   $("#signup-box-link").removeClass("active");
+// });
+});
